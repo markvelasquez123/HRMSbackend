3ad70c61f1c338074684f52c3e85311d6e823d44
markvelasquez123 <markvelasquez347@gmail.com>
2025-09-16 18:08:49 -0700

    bago

---
+++ rules.php
@@ -0,0 +1,40 @@
+<?php
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Methods: POST, OPTIONS, GET");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header('Content-Type: application/json');
+header("Access-Control-Allow-Credentials: true");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
+
+$host = 'localhost';
+$dbname = 'hrms';
+$username = 'root';
+$password = '';
+$conn = new mysqli($host, $username, $password, $dbname);
+if ($conn->connect_error) {
+    http_response_code(500);
+    echo json_encode(['error' => 'Database connection failed']);
+    exit();
+}
+
+
+$sql = "SELECT 
+    *
+    FROM rules";
+$result = $conn->query($sql);
+
+$employees = [
+   
+];
+if ($result && $result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        $employees[] = $row;
+    }
+}
+echo json_encode($employees);
+exit;
+?>
